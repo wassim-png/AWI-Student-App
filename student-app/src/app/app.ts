1be8531d83cfd35106134b15c7f88897f9430002
Wassim Ben Nacef <wassim.ben-nacef01@etu.umontpellier.fr>
2025-09-24 19:16:08 +0200

    Creation du composant hello-student et affichage de son contenu

--- student-app/src/app/app.ts
+++ student-app/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloStudent } from "../student/hello-student/hello-student";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloStudent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ student-app/src/student/hello-student/hello-student.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-student',
+  imports: [],
+  templateUrl: './hello-student.html',
+  styleUrl: './hello-student.css'
+})
+export class HelloStudent {
+
+}

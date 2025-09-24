aa75a7708facf3f4fab13f36bf186e7e17c198ef
Wassim Ben Nacef <wassim.ben-nacef01@etu.umontpellier.fr>
2025-09-24 21:22:15 +0200

    Affichage

--- student-app/src/app/app.ts
+++ student-app/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloStudent } from "../student/hello-student/hello-student";
+import { StudentCard } from "../student/student-card/student-card";
+import { StudentList } from "../student/student-list/student-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloStudent],
+  imports: [RouterOutlet, HelloStudent, StudentCard, StudentList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- student-app/src/main.ts
+++ student-app/src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+// Enregistre la locale française
+registerLocaleData(localeFr);
+
+bootstrapApplication(App);
